2cd9fe802590a5449f77a60b7849f96948ece234
Kavithvajen <kavith@kavithvajens-macbook-pro.local>
2017-12-26 18:01:03 +0530

    Converted to MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kavithvajen on 26/12/17.
+//  Copyright © 2017 Kavithvajen. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
